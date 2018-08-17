9041477486ef15a6d25a0d964b6d05939c25de1f
Julien Semaan <jul.semaan@gmail.com>
2018-08-16 21:36:41 -0400

    update afn-dist

--- afn-dist/snippets/pdms_pml.js
+++ afn-dist/snippets/pdms_pml.js
@@ -1,1 +1,9 @@
-define("ace/snippets/pdms_pml",["require","exports","module"],function(e,t,n){"use strict";t.snippetText=undefined,t.scope=""})
+define("ace/snippets/pdms_pml",["require","exports","module"],function(e,t,n){"use strict";t.snippetText=undefined,t.scope=""});
+                (function() {
+                    window.require(["ace/snippets/pdms_pml"], function(m) {
+                        if (typeof module == "object" && typeof exports == "object" && module) {
+                            module.exports = m;
+                        }
+                    });
+                })();
+            
